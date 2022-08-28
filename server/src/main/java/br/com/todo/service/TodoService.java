2fe85136031d6ff34f380b668aa98e4499abb586
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2022-08-28 20:11:20 -0300

    back

--- server/src/main/java/br/com/todo/service/TodoService.java
+++ server/src/main/java/br/com/todo/service/TodoService.java
@@ -10,8 +10,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 public class TodoService {
